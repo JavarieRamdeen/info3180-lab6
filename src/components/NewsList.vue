44594deacb6aae85e32c9f66c313a5a4c543a484
Javarie Ramdeen <JavarieRamdeen@gmail.com>
2022-04-04 13:57:25 -0500

    list items

---
+++ src/components/NewsList.vue
@@ -0,0 +1,33 @@
+<template>
+ <ul class="news__list">
+ <li class="news__item">News item 1</li>
+ <li class="news__item">News item 2</li>
+ <li class="news__item">News item 3</li>
+ </ul>
+</template>
+<script>
+export default {
+    data() {
+         return {
+            articles: []
+        }
+    },
+    created() {
+        let self = this;
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+    {
+    headers: {
+        'Authorization': 'Bearer <d0b55a1e99df44eb89753c297fa5f7c4>'
+    }
+})
+    .then(function(response) {
+        return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+        });
+    }
+}
+</script>
